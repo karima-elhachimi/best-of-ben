9a2c73eef890eac19037faef10d2a7f0a6afe4d8
karima-elhachimi <yuakarima@Yuas-MBP.lan>
2018-06-19 18:07:11 +0200

    Finishing touches regarding backgrund and text

--- ben quotes/ViewController.swift
+++ ben quotes/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lblQuote: UILabel!
+    @IBOutlet weak var imageAchtergrond: UIImageView!
     
     var rdmIndex : Int = 0;
+    var rdmImageIndex : Int = 0;
+    let achtergronden = [ #imageLiteral(resourceName: "achtergrond1"), #imageLiteral(resourceName: "achtergrond2") , #imageLiteral(resourceName: "achtergrond3"), #imageLiteral(resourceName: "achtergrond4")]
     
+   
     let benQuotes = ["Ik denk dat ik echt wel epic ben.",
                      "Sorry, door de pet kan je mijn wenkbrauwen niet goed zien!",
                      "Ik heb al 8 uur luistertijd op mijn radio.",
@@ -23,6 +27,8 @@ class ViewController: UIViewController {
     
     func generateQuote() {
         genRdmIndex()
+        imageAchtergrond.image = achtergronden[rdmImageIndex]
+        
         lblQuote.text = benQuotes[rdmIndex];
     }
     
@@ -30,11 +36,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         genRdmIndex()
+        lblQuote.adjustsFontSizeToFitWidth = true;
         
     }
 
     func genRdmIndex() {
         rdmIndex = Int(arc4random_uniform(UInt32(benQuotes.count)));
+        rdmImageIndex = Int(arc4random_uniform(UInt32(achtergronden.count)));
     }
   
     override func didReceiveMemoryWarning() {
